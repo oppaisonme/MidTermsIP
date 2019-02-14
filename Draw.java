7471dd3b2e5d75b761ea16e4aec37302a7119394
oppaisonme <jay.fernando.m@gmail.com>
2019-02-14 14:48:04 +0800

    2

--- Draw.java
+++ Draw.java
@@ -6,39 +6,28 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.net.URL;
+import java.util.Random;
+import java.util.ArrayList;
 
 public class Draw extends JComponent{
 
-	private BufferedImage image;
-	private BufferedImage backgroundImage;
-	public URL resource = getClass().getResource("run0.png");
+	public BufferedImage backgroundImage;
+	public Player player;
 
-	// circle's position
-	public int x = 30;
-	public int y = 30;
+	public Random randomizer;
 
-	// animation states
-	public int state = 0;
-
-	Monster monster1;
-	Monster monster2;
-	Monster monster3;
-	Monster monster4;
-	Monster monster5;
-	Monster monster6;
-	Monster monster7;
+	public int enemyCount;
+	Monster[] monsters = new Monster[10];
+	public ArrayList<Monster> monsterlist = new ArrayList<>();
+	
 
 	public Draw(){
-		monster1 = new Monster(200, 500);
-		monster2 = new Monster(250, 450);
-		monster3 = new Monster(300, 400);
-		monster4 = new Monster(350, 350);
-		monster5 = new Monster(400, 300);
-		monster6 = new Monster(450, 250);
-		monster7 = new Monster(500, 200);
+		randomizer = new Random();
+		player = new Player(-10, 520, this);
+
+		spawnEnemy();
 
 		try{
-			image = ImageIO.read(resource);
 			backgroundImage = ImageIO.read(getClass().getResource("background.jpg"));
 		}
 		catch(IOException e){
@@ -46,108 +35,56 @@ public class Draw extends JComponent{
 		}
 	}
 
-	public void reloadImage(){
-		state++;
-
-		if(state == 0){
-			resource = getClass().getResource("run0.png");
-		}
-		else if(state == 1){
-			resource = getClass().getResource("run1.png");
-		}
-		else if(state == 2){
-			resource = getClass().getResource("run2.png");
-		}
-		else if(state == 3){
-			resource = getClass().getResource("run3.png");
-		}
-		else if(state == 4){
-			resource = getClass().getResource("run4.png");
-		}
-		else if(state == 5){
-			resource = getClass().getResource("run5.png");
-			state = 0;
-		}
-
-		try{
-			image = ImageIO.read(resource);
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-	}
-
-	public void attackAnimation(){
-		Thread thread1 = new Thread(new Runnable(){
+	public void startGame(){
+		Thread gameThread = new Thread(new Runnable(){
 			public void run(){
-				for(int ctr = 0; ctr < 5; ctr++){
-					try {
-						if(ctr==4){
-							resource = getClass().getResource("run0.png");
+				while(true){
+					try{
+						for(int c = 0; c < monsterlist.size(); c++){
+							if(monsterlist.size()!= 0){
+								monsterlist.get(c).moveTo(player.x, player.y);
+								repaint();
+							}
+							if(monsterlist.get(c).life <= 0){
+								monsterlist.remove(c);
+				}
 						}
-						else{
-							resource = getClass().getResource("attack"+ctr+".png");
-						}
-						
-						try{
-							image = ImageIO.read(resource);
-						}
-						catch(IOException e){
-							e.printStackTrace();
-						}
-				        repaint();
-				        Thread.sleep(100);
+						Thread.sleep(100);
 					} catch (InterruptedException e) {
-						e.printStackTrace();
+							e.printStackTrace();
 					}
 				}
 			}
 		});
-		thread1.start();
+		gameThread.start();
 	}
 
-	public void attack(){
-		attackAnimation();
+	public void spawnEnemy(){
+		if(enemyCount < 10){
+			int random = randomizer.nextInt(600);
+			if(random < 600 || random < 30){
+			monsters[enemyCount] = new Monster(random, 530, this);
+			monsterlist.add(monsters[enemyCount]);
+			enemyCount++;
+			}
+		}
 	}
 
-	public void moveUp(){
-		y = y - 5;
-		reloadImage();
-		repaint();
-	}
 
-	public void moveDown(){
-		y = y + 5;
-		reloadImage();
-		repaint();
-	}
-
-	public void moveLeft(){
-		x = x - 5;
-		reloadImage();
-		repaint();
-	}
-
-	public void moveRight(){
-		x = x + 5;
-		reloadImage();
-		repaint();
-	}
-
-	
-	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(Color.YELLOW);
 		g.drawImage(backgroundImage, 0, 0, this);
-		g.drawImage(image, x, y, this);
+		
+		g.drawImage(player.image, player.x, player.y, this);
 
-		g.drawImage(monster1.image, monster1.xPos, monster1.yPos, this);
-		g.drawImage(monster2.image, monster2.xPos, monster2.yPos, this);
-		g.drawImage(monster3.image, monster3.xPos, monster3.yPos, this);
-		g.drawImage(monster4.image, monster4.xPos, monster4.yPos, this);
-		g.drawImage(monster5.image, monster5.xPos, monster5.yPos, this);
-		g.drawImage(monster6.image, monster6.xPos, monster6.yPos, this);
-		g.drawImage(monster7.image, monster7.xPos, monster7.yPos, this);	
+
+		for(int c = 0; c < monsterlist.size(); c++){
+			if(monsterlist.size() != 0){
+				g.drawImage(monsterlist.get(c).image, monsterlist.get(c).xPos, monsterlist.get(c).yPos, this);
+				g.setColor(Color.GREEN);
+				g.fillRect(monsterlist.get(c).xPos+7, monsterlist.get(c).yPos, monsterlist.get(c).life, 2);
+			}
+		}
 	}
 }
